9cbcd0ce82e58d122eda93e5b4f3faebe6221c87
ZubairBasharat <43141677+ZubairBasharat@users.noreply.github.com>
2022-05-09 13:56:01 +0500

    add link

--- resources/views/personal_dashboard.blade.php
+++ resources/views/personal_dashboard.blade.php
@@ -148,7 +148,7 @@
         </div>
       </div>
       <div class="center flex-wrap flex-md-nowrap">
-        <button class="theme-btn me-md-2">Start action planning</button>
+        <a href="{{url('myActionPlans')}}"><button class="theme-btn me-md-2">Start action planning</button></a>
         <button class="theme-btn hover w-238 mt-4 mt-md-0">Export Report</button>
       </div>
     </section>
