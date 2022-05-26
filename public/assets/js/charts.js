743820d6a5a79903fa9d1f685ba56a7762301b50
ZubairBasharat <43141677+ZubairBasharat@users.noreply.github.com>
2022-05-27 03:04:54 +0500

    make change in chart

--- public/assets/js/charts.js
+++ public/assets/js/charts.js
@@ -1,66 +1,6 @@
-const ctx1 = document.getElementById("meaning-chart").getContext("2d");
-const myChart1 = new Chart(ctx1, {
-    type: "bar",
-    data: {
-        labels: ["Your Score", "Others in the world"],
-        datasets: [
-        {
-            label: "Feeling Of Overall Meaning",
-            data: [5.0, 4.3],
-            backgroundColor: ["#ED1846", "#F15A22"],
-            borderColor: ["#ED1846", "#F15A22"],
-            borderWidth: 1,
-            borderRadius: 4,
-        },
-        ],
-    },
-    options: {
-        indexAxis: "y",
-        responsive: true,
-        plugins: {
-        legend: {
-            display: false,
-            position: "right",
-        },
-        title: {
-            display: true,
-            text: "Feeling Of Overall Meaning",
-        },
-        },
-    },
-    });
 
-    const ctx2 = document.getElementById("progress-chart").getContext("2d");
-    const myChart2 = new Chart(ctx2, {
-    type: "bar",
-    data: {
-        labels: ["Your Score", "Others in the world"],
-        datasets: [
-        {
-            label: "Feeling Of Overall Meaning",
-            data: [5.0, 4.3],
-            backgroundColor: ["#ED1846", "#F15A22"],
-            borderColor: ["#ED1846", "#F15A22"],
-            borderWidth: 1,
-            borderRadius: 4,
-        },
-        ],
-    },
-    options: {
-        indexAxis: "y",
-        responsive: true,
-        plugins: {
-        legend: {
-            display: false,
-            position: "right",
-        },
-        title: {
-            display: true,
-            text: "Feeling Of Overall Meaning",
-        },
-        },
-    },
-    });
+
+
 
     const ctx3 = document.getElementById("meaning-group-chart").getContext("2d");
 
