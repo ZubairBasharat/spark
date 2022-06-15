77ebd6d712ff7d69a5875a5519b9c3ee6f626e96
ZubairBasharat <43141677+ZubairBasharat@users.noreply.github.com>
2022-06-15 15:43:40 +0500

    check

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::middleware(['verified_token'])->group(function () {
     Route::get('/', function () {
         return view('about_us');
