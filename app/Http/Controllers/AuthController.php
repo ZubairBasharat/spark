cb8f0053b86dcbfb9c15df3e318df53cbb8f50ba
abdullah01 <abdullah@repairdesk.co>
2022-07-26 14:15:01 +0500

    Abdullah Chnages base url

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,8 @@ use Psy\CodeCleaner\IssetPass;
 
 class AuthController extends Controller
 {
-    public $base_url = "https://testing.sparkdlens.com";
+    // public $base_url = "https://testing.sparkdlens.com";
+    public $base_url = "http://3.97.226.161";
     public function login(Request $request)
     {
         $validatedData = $request->validate([
